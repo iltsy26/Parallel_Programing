d308c882aef2bc4492affeded7301123cf9ecd54
iltsy26 <71667539+iltsy26@users.noreply.github.com>
2022-12-26 22:28:27 +0300

    Add files via upload

---
+++ PP_Lab13/PP_Lab13/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_Lab13
+{
+    public class TaskResult
+    {
+        public int[,] mas;
+
+        public int tSumOfElDivBy4 { get
+            {
+                int SumOfElms = 0;
+                for (int i = 0; i < mas.GetLength(0); i++)
+                {
+                    for (int j = 0; j < mas.GetLength(1); j++)
+                    {
+                        if (mas[i, j] % 4 == 0)
+                            SumOfElms += mas[i, j];
+                    }
+                }
+
+                return SumOfElms;
+            } 
+        }
+
+        public int tNumOfElDivBy3
+        {
+            get
+            {
+                int NumOfElms = 0;
+                for (int i = 0; i < mas.GetLength(0); i++)
+                {
+                    for (int j = 0; j < mas.GetLength(1); j++)
+                    {
+                        if (mas[i, j] % 3 == 0)
+                            NumOfElms++;
+                    }
+                }
+
+                return NumOfElms;
+            }
+        }
+
+        public int tMinEl
+        {
+            get
+            {
+                int MinEl = mas[0, 0];
+                for (int i = 0; i < mas.GetLength(0); i++)
+                {
+                    for (int j = 0; j < mas.GetLength(1); j++)
+                    {
+                        if (mas[i, j] <= MinEl)
+                            MinEl = mas[i, j];
+                    }
+                }
+
+                return MinEl;
+            }
+        }
+    }
+
+    internal class Program
+    {
+        public static TaskResult MethodForTask(object dim)
+        {
+            TaskResult result = new TaskResult();
+            int dimCount = (int)dim;
+
+            result.mas = new int[dimCount, dimCount];
+            Random rand = new Random();
+            for (int i = 0; i < dimCount; i++)
+            {
+                for (int j = 0; j < dimCount; j++)
+                {
+                    result.mas[i, j] = rand.Next(1000) - 200;
+                }
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            var TaskWithResult = new Task<TaskResult>(MethodForTask, 4);
+            TaskWithResult.Start();
+            int[,] mas = TaskWithResult.Result.mas;
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                for (int j = 0; j < mas.GetLength(1); j++)
+                {
+                    Console.Write(mas[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Sum of elements divisible by 4 = {0} " +
+                "\nNumber of elements divisible by 3 = {1} " +
+                "\nMimimum element = {2} ",
+                TaskWithResult.Result.tSumOfElDivBy4, 
+                TaskWithResult.Result.tNumOfElDivBy3, 
+                TaskWithResult.Result.tMinEl);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PP_Lab14/PP_Lab14/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PP_Lab14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int x = 3, y = 5, z = 2;
+            int[,,] matrix = new int[y, x, z];
+            Random random = new Random();
+            Console.WriteLine("Initialization: ");
+            ParallelLoopResult res = Parallel.For(0, x, j =>
+            {
+                for (int i = 0; i < y; i++)
+                {
+                    for (int t = 0; t < z; t++)
+                    { 
+                        matrix[i, j, t] = random.Next(1000);
+                        Console.WriteLine("matrix [{0}, {1}, {2}] = {3} \t Thread: {4} \t Task {5}",
+                        i, j, t, matrix[i, j, t],
+                        Thread.CurrentThread.ManagedThreadId, Task.CurrentId);
+                    }
+                }
+            });
+            Console.WriteLine("Initialization finished - {0}\n", res.IsCompleted);
+            int MinElofMass = matrix[0, 0, 0];
+            res = Parallel.For(0, y, i =>
+            {
+                int Min = matrix[i, 0, 0];
+                for (int j = 0; j < x; j++)
+                {
+                    for (int k = 0; k < z; k++)
+                    {
+                        if (Min > matrix[i, j, k])
+                            Min = matrix[i, j, k];
+                    }
+                }
+                if (MinElofMass > Min)
+                    MinElofMass = Min;
+            });
+
+            Console.WriteLine("Min in mass {0}", MinElofMass);
+
+            Console.ReadLine();
+        }
+    }
+}
